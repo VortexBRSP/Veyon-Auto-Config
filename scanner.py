5243239a89b43cbbf2095d363f7b041644133fdc
Eduardo Lima <edulimacosta223@gmail.com>
2022-06-10 12:11:35 -0300

    Mensagem de boas vindas adicionada, loop de programa adicionado

--- scanner.py
+++ scanner.py
@@ -7,12 +7,15 @@ def gerarIp(ipBase, qtdIps):
             os.system('veyon-cli networkobjects add location Lab {}.{} "" ""'.format(ipBase, i))
         except:
             print("[ERRO] IP INVÁLIDO ERRO AO ADICIONAR!")
-try:
-    ipBase = str(input("Digite as três primeiras seções do seu IP Local EX: 192.168.2\n"))
-    qtdIps = int(input("Digite a quantidade de IPs para gerar( Recomendado: 200)\n"))
-    gerarIp(ipBase, qtdIps)
-    input("Ip's adicionado pressione qualquer tecla para encerrar...")
-except:
-    print("[ERRO] DADOS INVALIDOS TENTE NOVAMENTE\n")
-    input("Pressione qualquer tecla para encerrar...")
+print(".--.\n|__| .-------.\n|=.| |.-----.|\n|--| || VAS ||\n|  | |'-----'|\n|__|~')_____('\nVAS 2.0\nAuthor: @vortexbrsp")
+while True:
+    try:
+
+        ipBase = str(input("Digite as três primeiras seções do seu IP Local EX: 192.168.2\n"))
+        qtdIps = int(input("Digite a quantidade de IPs para gerar( Recomendado: 200)\n"))
+        gerarIp(ipBase, qtdIps)
+        input("Ip's adicionado pressione qualquer tecla para encerrar...")
+        break
+    except:
+        print("[ERRO] DADOS INVALIDOS TENTE NOVAMENTE\n")
 
