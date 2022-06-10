79e928b6ff30dc0cabb9d054c55537acdc0e7aa1
Eduardo Lima <edulimacosta223@gmail.com>
2022-06-10 12:35:44 -0300

    Adicionado execução como administrado para correção de bugs

--- scanner.py
+++ scanner.py
@@ -1,23 +1,35 @@
 import os
+import ctypes, sys
 
-def gerarIp(ipBase, qtdIps):
-    for i in range(0, qtdIps):
-        print("{}.{} Adicionando IP.".format(ipBase, i))
-        try:
-            os.system('veyon-cli networkobjects add location Lab {}.{} "" ""'.format(ipBase, i))
-        except:
-            print("[ERRO] IP INVÁLIDO ERRO AO ADICIONAR!")
-
-
-print(".--.\n|__| .-------.\n|=.| |.-----.|\n|--| || VAS ||\n|  | |'-----'|\n|__|~')_____('\nVAS 2.0\nAuthor: @vortexbrsp")
-
-while True:
+def is_admin():
     try:
-        ipBase = str(input("Digite as três primeiras seções do seu IP Local EX: 192.168.2\n"))
-        qtdIps = int(input("Digite a quantidade de IPs para gerar( Recomendado: 200)\n"))
-        gerarIp(ipBase, qtdIps)
-        input("Ip's adicionado pressione qualquer tecla para encerrar...")
-        break
+        return ctypes.windll.shell32.IsUserAnAdmin()
     except:
-        print("[ERRO] DADOS INVALIDOS TENTE NOVAMENTE\n")
+        return False
+
+if is_admin():
+    def gerarIp(ipBase, qtdIps):
+        for i in range(0, qtdIps):
+            print("{}.{} Adicionando IP.".format(ipBase, i))
+            try:
+                os.system('veyon-cli networkobjects add location Lab {}.{} "" ""'.format(ipBase, i))
+            except:
+                print("[ERRO] IP INVÁLIDO ERRO AO ADICIONAR!")
+
+    print(".--.\n|__| .-------.\n|=.| |.-----.|\n|--| || VAS ||\n|  | |'-----'|\n|__|~')_____('\nVAS 2.0\nAuthor: @vortexbrsp")
+
+    while True:
+        try:
+            ipBase = str(input("Digite as três primeiras seções do seu IP Local EX: 192.168.2\n"))
+            qtdIps = int(input("Digite a quantidade de IPs para gerar( Recomendado: 200)\n"))
+            gerarIp(ipBase, qtdIps)
+            input("Ip's adicionado pressione qualquer tecla para encerrar...")
+            break
+        except:
+            print("[ERRO] DADOS INVALIDOS TENTE NOVAMENTE\n")
+else:
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+
+
+
 
