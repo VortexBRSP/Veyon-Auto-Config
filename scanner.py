b244f4db3773224615185fdb82ac14a850996bd3
Eduardo Lima <edulimacosta223@gmail.com>
2022-06-10 11:56:32 -0300

    Função de adicionar IP criada e adicionado a entrada de dados

--- scanner.py
+++ scanner.py
@@ -1,6 +1,12 @@
 import os
+
 def gerarIp(ipBase, qtdIps):
     for i in range(0, qtdIps):
-        print("{}.{}".format(ipBase, i))
+        print("{}.{} Adicionando IP.".format(ipBase, i))
+        try:
+            os.system('veyon-cli networkobjects add location Lab {}.{} "" ""'.format(ipBase, i))
+        except:
+            print("[ERRO] IP INVÁLIDO ERRO AO ADICIONAR!")
 
-gerarIp("192.168.0",200)
+ipBase = input("Digite as três primeiras seções do seu IP Local EX: 192.168.2\n")
+qtdIps = input("Digite a quantidade de IPs para gerar( Recomendado: 200)\n")
