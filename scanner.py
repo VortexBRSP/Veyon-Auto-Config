5706bf4bc5a06c9f0bfb507d61d726db8cd4d10d
Eduardo Lima <edulimacosta223@gmail.com>
2022-06-10 12:18:42 -0300

    Salvando ponto para alteração

--- scanner.py
+++ scanner.py
@@ -7,10 +7,12 @@ def gerarIp(ipBase, qtdIps):
             os.system('veyon-cli networkobjects add location Lab {}.{} "" ""'.format(ipBase, i))
         except:
             print("[ERRO] IP INVÁLIDO ERRO AO ADICIONAR!")
+
+
 print(".--.\n|__| .-------.\n|=.| |.-----.|\n|--| || VAS ||\n|  | |'-----'|\n|__|~')_____('\nVAS 2.0\nAuthor: @vortexbrsp")
+
 while True:
     try:
-
         ipBase = str(input("Digite as três primeiras seções do seu IP Local EX: 192.168.2\n"))
         qtdIps = int(input("Digite a quantidade de IPs para gerar( Recomendado: 200)\n"))
         gerarIp(ipBase, qtdIps)
